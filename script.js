120c6b12f7cc84cbf4b9c2906ce3393154df48f5
Seungvin <binii030124@naver.com>
2026-06-16 16:32:53 +0900

    클로드 모델 지원중단에 따른 모델 변경

--- script.js
+++ script.js
@@ -477,7 +477,7 @@ async function translateSubtitles(subs) {
                         'anthropic-dangerous-direct-browser-access': 'true'
                     },
                     body: JSON.stringify({
-                        model: 'claude-sonnet-4-20250514',
+                        model: 'claude-sonnet-4-6',
                         //model: 'claude-3-5-haiku-latest', 
                         max_tokens: 4096,
                         
@@ -567,7 +567,7 @@ async function translateSubtitles(subs) {
                         'anthropic-dangerous-direct-browser-access': 'true'
                     },
                     body: JSON.stringify({
-                        model: 'claude-sonnet-4-20250514',
+                        model: 'claude-sonnet-4-6',
                         max_tokens: 4096,
                         system: [{
                             type: "text",
